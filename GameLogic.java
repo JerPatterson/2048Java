5d33cd7a2a8cbe98d3e339c7eed47090bee8ec3b
JerPatterson <jeremie.patterson@polymtl.ca>
2022-05-13 21:21:29 -0400

    end of game verification draft writen

--- GameLogic.java
+++ GameLogic.java
@@ -1,10 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class GameLogic {
     private static final int LENGHT = 4;
     private static final int HEIGHT = 4;
 
+    private static final char RIGHT_MOVE = 'f';
+    private static final char LEFT_MOVE = 's';
+    private static final char UP_MOVE = 'e';
+    private static final char DOWN_MOVE = 'd';
+    
+
+    Scanner reader = new Scanner(System.in);
     private int[][] grid = new int[LENGHT][HEIGHT];
 
 
@@ -18,9 +26,12 @@ public class GameLogic {
         print();
 
         // TEMPORARY (to test):
-        System.out.println("\nNouvelle case (test):\n");
-        generateRandomNewSquares();
+        waitForMove();
         print();
+        waitForMove();
+        print();
+
+        reader.close();
     }
 
 
@@ -67,6 +78,94 @@ public class GameLogic {
             return 2;
         else if (randomNumber < newSquarePercentage)
             return 4;
+
+        return 0;
+    }
+
+
+    public void waitForMove() {
+        if (!isEnd()) {
+            reader.useDelimiter("");
+            char newMove = reader.nextLine().charAt(0);
+            
+            makeMove(newMove);
+            generateRandomNewSquares();
+        }
+
+    }
+
+    private boolean isEnd() {
+        for (int i = 0; i < LENGHT; ++i) {
+            for (int j = 0; j < HEIGHT; ++j) {
+                if (isValidHorizontalMove(i, j) 
+                    || isValidVerticalMove(i, j)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
+
+    private void makeMove(char move) {
+        if (move == RIGHT_MOVE) {
+
+        }
+
+        else if (move == LEFT_MOVE) {
+
+        }
+
+        else if (move == UP_MOVE) {
+
+        }
+
+        else if (move == DOWN_MOVE) {
+
+        }
+    }
+
+
+    private boolean isValidHorizontalMove(int row, int column) {
+        if (grid[row][column] == 0) {
+            return true;
+        }
+        else if (getNextInRow(row, column) == grid[row][column]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isValidVerticalMove(int row, int column) {
+        if (grid[row][column] == 0) {
+            return true;
+        }
+        else if (getNextInColumn(row, column) == grid[row][column]) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private int getNextInRow(int row, int column) {
+        for (int i = column; i < LENGHT; ++i) {
+            if (grid[row][i] != 0) {
+                return grid[row][i];
+            }
+        }
+
+        return 0;
+    }
+
+    private int getNextInColumn(int row, int column) {
+        for (int i = row; i < HEIGHT; ++i) {
+            if (grid[i][column] != 0) {
+                return grid[i][column];
+            }
+        }
 
         return 0;
     }
